1bc50c2d580d3fa4581ce84b4d85b545783e6900
Guillermo Cardoze <vital_tmv@yahoo.com>
2024-03-26 14:22:39 -0500

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining === 1){
+        setTimeRemaining(10)
+        onAnswered(false)
+      } else {
+        setTimeRemaining(timeRemaining -1)
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
